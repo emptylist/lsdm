dba1495e4b31c03ed07a8feee74fe77e93cca2c1
emptylist <crooks1379@gmail.com>
2013-03-23 20:37:35 -0500

    fixed distance function, added comments, and put core distance matrix generation function into scipy C routines.

--- lsdm.py
+++ lsdm.py
@@ -8,28 +8,37 @@ from scipy.spatial.distance import squareform
 
 ## TODO: CML file access to data
 
-## Currently this is functioning as a function library
+## Currently this is acting as a function library
 
-## The slowest part of this flow is constructing the
-## Probability Kernel.  Perhaps preprocessing the data
-## to reduce the calls will help.  Or writing the
-## metric function in C.
-
-def gaussianKernel(x,y):
-    v = x - y
-    return exp(-1*(np.dot(v,v))/2)
+## Furthermore this only implements Diffusion Maps,
+## *NOT* Locally Scaled DMs at this time.
 
 def constructProbabilityKernel(dataArray, eps):
-    return squareform((1/eps) * pdist(dataArray, gaussianKernel)) 
+    '''Generates a distance/probability matrix from the gaussian
+    kernel.  The probability matrix is not normalized, and does
+    not represent actual probabilities.  This is the major bottleneck
+    in the performance thus far.  Using kd-trees from either the 
+    scikit-ann or scikit-learn packages should help this.'''
+    return squareform((1/(2*eps)) * pdist(dataArray, 'sqeuclidean')) 
 
 def normalizeProbabilityKernel(probabilityKernel):
+    '''Normalizing the prob matrix is equivalent to 
+    approximating a density estimator under the 
+    kernel used for the distance function.  Small experiments
+    suggest this is efficient.'''
     P = np.apply_along_axis(np.sum, 1, probabilityKernel)
     return probabilityKernel / np.sqrt(np.outer(P,P))
 
 def constructMarkovMatrix(npKernel):
+    '''This produces a row-stochastic matrix from a positive
+    semi-definite matrix.'''
     return np.apply_along_axis(lambda v: v/sum(v), 1, npKernel)
 
 def diffusionMap(markovMatrix):
+    '''Performs eigenvalue decomposition on the Markov matrix.
+    This needs to be replaced with sparse methods and return
+    and object with the embedded data instead.'''
+    ## TODO: Return embedding instead.
     return np.eig(markovMatrix)
 
-## TODO: Output the data in the embedded manifold
+
