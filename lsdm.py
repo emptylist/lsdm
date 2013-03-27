4b2e8b9f806b5e632f379a5fa463d0dcb807388e
James Crooks <jcrooks@quicksilver.uchicago.edu>
2013-03-27 15:25:14 -0500

    Rewrote lsdm to use scikit-learn Spectral Embedding

--- lsdm.py
+++ lsdm.py
@@ -2,18 +2,37 @@
 
 import numpy as np
 import numpy.linalg as la
+
 from scipy import exp
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
+from sklearn.base import BaseEstimator, TransformerMixins
+from sklearn.manifold.spectral_embedding import spectral_embedding, SpectralEmbedding
+
 ## TODO: CML file access to data
+
+## Note to self: The Markov Matrix *IS* the normalized graph Laplacian
+## under the diffusion distance kernel.  Hence it suffices to build the
+## diffusion affinity matrix and simply pass that to scikit-learn's
+## Spectral Embedding.
+
+## Design question: Do I build a DiffusionMap object modeled on the
+## SpectralEmbedding object in scikit-learn, or write functions that
+## build the diffusion affinity matrix and pass that as a precomputed
+## affinity matrix to Spectral Embedding?  The first has a nicer and
+## more consistent interface, but the second prevents some unnecessary
+## code replication (with essentially copy&paste style).
 
 '''Currently this is acting as a function library.
 
 Furthermore this only implements Diffusion Maps,
 *NOT* Locally Scaled DMs at this time.'''
 
-def constructProbabilityKernel(dataArray, eps):
+def _local_scale_determination(dataArray):
+    pass
+
+def _constructProbabilityKernel(dataArray, eps):
     '''Generates a distance/probability matrix from the gaussian
     kernel.  The probability matrix is not normalized, and does
     not represent actual probabilities.  This is the major bottleneck
@@ -21,32 +40,29 @@ def constructProbabilityKernel(dataArray, eps):
     scikit-ann or scikit-learn packages should help this.'''
     return squareform(exp(-1*((1/(2*eps)) * pdist(dataArray, 'sqeuclidean'))))
 
-def normalizeProbabilityKernel(probabilityKernel):
-    '''Normalizing the prob matrix is equivalent to 
-    approximating a density estimator under the 
-    kernel used for the distance function.  Small experiments
-    suggest this is efficient.'''
-    P = np.apply_along_axis(np.sum, 1, probabilityKernel)
-    return probabilityKernel / np.sqrt(np.outer(P,P))
+def fitDiffusionMap(dataArray, eps, n_components=2):
+    X = _constructProbabilityKernel(dataArray, eps)
+    return SpectralEmbedding(n_components, affinity="precomputed").fit(X)
 
-def constructMarkovMatrix(npKernel):
-    '''This produces a row-stochastic matrix from a positive
-    semi-definite matrix.'''
-    return np.apply_along_axis(lambda v: v/sum(v), 1, npKernel)
+def fit_transformDiffusionMap(dataArray, eps, n_components=2):
+    X = _constructProbabilityKernel(dataArray, eps)
+    return SpectralEmbedding(n_components, affinity="precomputed").fit(X)
 
-def diffusionMap(markovMatrix):
-    '''Performs eigenvalue decomposition on the Markov matrix.
-    This needs to be replaced with sparse methods and return
-    and object with the embedded data instead.'''
-    ## TODO: Return embedding instead.
-    return np.eig(markovMatrix)
+class DiffusionMap(BaseEstimator, TransformerMixins):
+    '''Diffusion Map object styled on (and using code from) the scikit-learn
+    SpectralEmbedding object. Unfinished.'''
+    
+    def __init__(self, n_components=2, affinity="precomputed",
+                 gamma=None, random_state=None, eigen_solver=None,
+                 n_neighbors=None):
+        self.n_components = n_components
+        self.affinity = affinity
+        self.gamma = gamma
+        self.random_state = random_state
+        self.eigen_solver = eigen_solver
+        self.n_neighbors = n_neighbors
+        
 
-def fullDiffusionMaps(dataArray, eps):
-    K = exp(-1*squareform((1/(2*eps) * pdist(dataArray, 'sqeuclidean'))))
-    P = np.apply_along_axis(np.sum, 1, K)
-    Kbar = K / np.sqrt(np.outer(P,P))
-    M = np.apply_along_axis(lambda v: v/sum(v), 1, Kbar)
-    return np.eig(M)
 
 if __name__ == "__main__":
     print "TODO: Add commandline functionality!"
