9d657aba4278ac154e7aa70477a2ee2aada46b90
James Crooks <jcrooks@quicksilver.uchicago.edu>
2013-04-02 15:40:18 -0500

    Sparse operations
    Fixed some math errors in the kernel computation.  Should be correct
    up to passing the affinity matrix to spectral_embedding.  Also changed
    the kernel to compute final dot products with sparse matrices and return
    a sparse affinity matrix.  This shows massive speedups, but the embedding
    is still very slow.  Replacing the scikit-learn spectral_embedding function
    is probably correct, as it makes unneccesary, and possibly erroneous, assumptions
    about the problem it's being applied to.

--- lsdm.py
+++ lsdm.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.linalg as la
 
+import scipy.sparse
 from scipy import exp
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
@@ -12,20 +13,14 @@ from sklearn.manifold.spectral_embedding import spectral_embedding, SpectralEmbe
 
 ## TODO: CML file access to data
 
-## Note to self: The Markov Matrix *IS* the normalized graph Laplacian
-## under the diffusion distance kernel.  Hence it suffices to build the
-## diffusion affinity matrix and simply pass that to scikit-learn's
-## Spectral Embedding class or spectral_embedding function.
-
-## Design question: Do I build a DiffusionMap object modeled on the
-## SpectralEmbedding object in scikit-learn, or write functions that
-## build the diffusion affinity matrix and pass that as a precomputed
-## affinity matrix to Spectral Embedding?  The first has a nicer and
-## more consistent interface, but the second prevents some unnecessary
-## code replication (with essentially copy&paste style).
-
 ## ISSUE: It's currently not clear what the best interface is for integrating
 ## locally-scaled and fixed neighborhood diffusion maps into a single class
+
+## SPEED TEST: cProfile shows that the sparse matrices solved large dot-product problem,
+## decreased the embedding time.  However, symmetric matrix errors are popping up from
+## spectral_embedding and the sparse LU solver is still slow.  Best idea seems to be to
+## write a separate embedding process and use scipy.sparse.eigsh
+
 
 '''Currently this is acting as a function library.
 
@@ -54,19 +49,29 @@ def _constructProbabilityKernel(dataArray, eps):
     not represent actual probabilities.  This is the major bottleneck
     in the performance thus far.  Using kd-trees from either the 
     scikit-ann or scikit-learn packages should help this.'''
-    return squareform(exp(-1*((1/(2*eps)) * pdist(dataArray, 'sqeuclidean'))))
+    K = exp((-1/(2*eps)) * squareform(pdist(dataArray, 'sqeuclidean')))
+    threshold = 0.02*exp(-1/eps) ## To make the matrix sparse 
+    K[K < threshold] = 0
+    P = np.apply_along_axis(np.sum, 1, K)
+    Kbar = K / np.sqrt(np.outer(P,P))
+    D = np.diag(np.sqrt(1./np.apply_along_axis(np.sum, 1, Kbar)))
+    Kbar_spar = scipy.sparse.csr_matrix(Kbar)
+    D_spar = scipy.sparse.dia_matrix(D)
+    return np.dot(np.dot(D_spar,Kbar_spar),D_spar)
 
 class DiffusionMap(BaseEstimator, TransformerMixin):
     '''Diffusion Map object styled on (and using code from) the scikit-learn
     SpectralEmbedding object. '''
     
-    def __init__(self, n_components=2, local_scaling=False, eigen_solver=None):
+    def __init__(self, n_components=2, local_scaling=False, eigen_solver=None
+                 , epsilon=0.1):
         self.n_components = n_components
         self.local_scaling = local_scaling
         self.eigen_solver = eigen_solver
+        self.epsilon = epsilon
 
     def _get_affinity_matrix(self, X):
-        affinity_matrix = _constructProbabilityKernel(X)
+        affinity_matrix = _constructProbabilityKernel(X, self.epsilon)
         if self.local_scaling:
             print "Local scaling is not implemented yet. Reverting to standard diffusion maps."
         return affinity_matrix
@@ -75,7 +80,7 @@ class DiffusionMap(BaseEstimator, TransformerMixin):
     def fit(self, X):
         """Fit the model from data in X."""
         self.affinity_matrix_ = self._get_affinity_matrix(X)
-        self.embedding_ = spectral_embedding(affinity_matrix,
+        self.embedding_ = spectral_embedding(self.affinity_matrix_,
                                              n_components=self.n_components,
                                              eigen_solver=self.eigen_solver)
         return self
