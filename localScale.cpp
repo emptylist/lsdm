f81f0e05dc05ea6f2f2df10452882909bd211574
emptylist <crooks1379@gmail.com>
2013-07-05 11:00:08 -0500

    local scaling work

--- localScale.cpp
+++ localScale.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <stdexcept>
+#include <cmath>
+#include <vector>
+//#include "Eigen/Dense"
 #include "localScale.h"
+
+using namespace std;
 
 //functions for computing the local scaling parameters
 
@@ -8,7 +14,44 @@ bool compare(const DistArr& x, const DistArr& y)
   return x.dist < y.dist;
 }
 
-double distance(const Eigen::ArrayXd& x, const Eigen::ArrayXd& y)
+double euclidean(const vector<double>& x, const vector<double>& y)
 {
-  //fill in function
+  if (x.size()!=y.size())
+    throw domain_error("Vectors are not the same size.");
+
+  vector<double>::size_type sz = x.size();
+  double sqdist = 0;
+  
+  for (vector<double>::size_type i = 0; i < sz; ++i) {
+    sqdist += pow((x[i] - y[i]), 2);
+  }
+
+  return sqrt(sqdist);
+}
+
+DistArr dist_y_from_x(const vector<double>& x, const vector<double>& y)
+{
+  //return DistArr { dist: distance(x,y), arr: x& } ;
+}
+
+int main() 
+{
+  vector<double> x;
+  vector<double> y;
+
+  x.push_back(1.0);
+  x.push_back(1.0);
+  x.push_back(1.0);
+  x.push_back(1.0);
+
+  y.push_back(3.0);
+  y.push_back(3.0);
+  y.push_back(3.0);
+  y.push_back(3.0);
+
+  double d = euclidean(x,y);
+
+  cout << d << endl;
+
+  return 0;
 }
--- localScale.h
+++ localScale.h
@@ -1,14 +1,19 @@
 #ifndef LOCAL_SCALE_H
 #define LOCAL_SCALE_H
 
-#include "Eigen/Dense"
+#include <vector>
+//#include "Eigen/Dense"
 
 struct DistArr {
   double dist;
-  Eigen::ArrayXd arr;
+  std::vector<double> vec;
 };
 
 bool compare(const DistArr&, const DistArr&);
-double distance(const Eigen::ArrayXd&, const Eigen::ArrayXd&);
+double distance(const std::vector<double>&, const std::vector<double>&);
+DistArr dist_y_from_x(const std::vector<double>&, const std::vector<double>&);
+
+//double distance(const Eigen::ArrayXd&, const Eigen::ArrayXd&);
+//DistArr dist_y_from_x(const Eigen::ArrayXd&, const Eigen::ArrayXd&);
 
 #endif
